0d32f1fdc754186da33c03f33f23070550ee0ff7
Pianocraft1YT <lucca.chen@crpusd.org>
2026-01-09 13:25:33 -0800

    hotfix catching errors

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -5,7 +5,6 @@ from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
 def do_command(ip, option_selected):
-    
     if ip != "":
         if option_selected == "nslookup":
             command = ["nslookup", ip]
@@ -53,9 +52,19 @@ def do_command(ip, option_selected):
             lbl_wrong = tk.Label(frame_wrong, text="An Error Occured. Please double-check your selected options.", font="Times, 26")
             lbl_wrong.pack()
 def get_text():
-    ip = ip_entry.get()
-    option_selected = options_listbox.get()
-    do_command(ip, option_selected)
+    try:
+        ip = ip_entry.get()
+        option_selected = options_listbox.get()
+        do_command(ip, option_selected)
+    except BaseException:
+        root1 = tk.Tk()
+        root1.wm_geometry("200x200")
+        root1.title("An Error Occured")
+        frame_wrong = tk.Frame(root1)
+        frame_wrong.grid()
+    
+        lbl_wrong = tk.Label(frame_wrong, text="An Error Occured. Please double-check your selected options.", font="Times, 26")
+        lbl_wrong.pack()
 def mSave():
   filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
   if filename is None:

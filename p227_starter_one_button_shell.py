6b54136c383b1e1ba076375f84990a03ddf4224f
Pianocraft1YT <lucca.chen@crpusd.org>
2026-01-09 13:01:50 -0800

    added save output button, output to tkinter box

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -30,29 +30,46 @@ def do_command(ip, option_selected):
         root1.title("No IP destined")
         frame_wrong = tk.Frame(root1)
         frame_wrong.grid()
-    
         lbl_wrong = tk.Label(frame_wrong, text="Please enter an IP address.", font="Times, 26")
         lbl_wrong.pack()
     # Mac version to limit to 4 requests:     command = ["ping", "localhost", "-n", "4"]
+    global command_textbox
     
-    subprocess.run(command)
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.update()
+
+    with subprocess.Popen(command,stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            command_textbox.insert(tk.END,line)
+            command_textbox.update()
 def get_text():
     ip = ip_entry.get()
     option_selected = options_spinbox.get()
     do_command(ip, option_selected)
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  file.write(text_to_save)
+  file.close()
+
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=get_text)
+execute_btn = tk.Button(frame, text="Execute Operation", command=get_text)
 ip = ""
 options = ('ping', 'nslookup', 'tracert', 'nmap')
 ip_entry = tk.Entry(frame, width=16, textvariable=ip)
 command_textbox = tksc.ScrolledText(frame, height=10, width=100) 
-options_spinbox = tk.Spinbox(frame, values=options)
+options_spinbox = tk.Spinbox(frame, values=options, state="readonly")
+save_output_btn = tk.Button(frame, text="Save Output", command=mSave)
 ip_entry.pack()
-ping_btn.pack()
-command_textbox.pack()
+execute_btn.pack()
+save_output_btn.pack()
 options_spinbox.pack()
+command_textbox.pack()
 root.mainloop()
